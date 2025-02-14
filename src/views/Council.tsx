c5db468e4b080a8b6c33e571ab5764f4e3e95250
Mark Luiz <markbobis173@gmail.com>
2025-02-14 08:29:38 +0800

    done council front

--- src/views/Council.tsx
+++ src/views/Council.tsx
@@ -3,10 +3,22 @@ import NavBar from "../components/NavBar";
 import { useEffect, useState } from "react";
 import { ICouncil } from "../../interface/council";
 import { data } from "../../data/councils";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Council() {
   const { id } = useParams();
   const [council, setCouncile] = useState<ICouncil | undefined>(undefined);
+
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  }, []);
+
   useEffect(() => {
     if (!id) {
       return;
@@ -16,14 +28,15 @@ function Council() {
     setCouncile(getCouncil);
   }, [id]);
 
-  console.log(council);
-
   return (
     <div className="max-w-[2000px] mx-auto h-screen w-screen min-w-screen min-h-screen">
       <NavBar />
 
       <div className="h-full w-full  pt-[85px] flex flex-col md:flex-row justify-center gap-8">
-        <div className="flex flex-col items-center justify-center px-16">
+        <div
+          className="flex flex-col items-center justify-center px-16"
+          data-aos="fade-right"
+        >
           <img
             src="/images/profile.jpg"
             alt="Profile"
@@ -41,7 +54,10 @@ function Council() {
             </>
           )}
         </div>
-        <div className="flex-1 h-full  lg:max-w-[750px] grid place-items-center">
+        <div
+          className="flex-1 h-full  lg:max-w-[750px] grid place-items-center"
+          data-aos="fade-left"
+        >
           {council && (
             <p className="p-4 md:p-10 lg:p-16 text-white  text-xl uppercase font-bold md:bg-[var(--blue)] rounded-4xl">
               {council.vow}
